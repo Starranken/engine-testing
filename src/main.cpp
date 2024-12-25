c24f434c98ca71bfbce4688dc99157b2aedd22e2
Starranken <xlecluse908@gmail.com>
2024-12-24 22:36:58 -0800

    Built up base for the code
    Fix library referencing!!!!

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>   //include the file access ibrary
+#include <vector>
+#include <string>   //include the strng library
+#include <cmath>    //include the math library
+#include <vector>   //include the vector library
+#include <ctime>    //include the time library
+
+using namespace std;
+
+int main () {
+    cout << "Hello World!" << endl;
+    return 0;
+};
